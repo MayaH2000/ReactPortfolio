815fda84f6c65f51c1f997b44d1e5df7b6d492e3
MayaH2000 <101356128+MayaH2000@users.noreply.github.com>
2024-02-19 16:32:16 -0600

    updated contact information

--- MayaH2000-React-Portfolio/src/components/pages/Contact.jsx
+++ MayaH2000-React-Portfolio/src/components/pages/Contact.jsx
@@ -1,19 +1,17 @@
+// Contact component providing information to contact Maya Marie Hernandez
 export default function Contact() {
   return (
     <div>
-      <h1>Contact Page</h1>
+      {/* Contact details */}
+      <h1>Contact Maya Marie Hernandez</h1>
       <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
+        You can reach out to Maya Marie Hernandez for inquiries or collaborations.
+        <br />
+        GitHub: <a href="https://github.com/MayaH2000">MayaH2000</a>
+        <br />
+        Email: <a href="mailto:MayaMarie_Offic@aol.com">MayaMarie_Offic@aol.com</a>
+        <br />
+        Availability: 7:00 AM - 7:00 PM CST
       </p>
     </div>
   );
