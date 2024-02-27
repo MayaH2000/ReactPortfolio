74e1033ee5471a8d7f7dd78c2d277e265af1a64e
MayaH2000 <101356128+MayaH2000@users.noreply.github.com>
2024-02-27 00:08:47 -0600

    reverted app.jsx, created app.css file and replaced former contents

--- MayaH2000-React-Portfolio/src/App.jsx
+++ MayaH2000-React-Portfolio/src/App.jsx
@@ -1,10 +1,27 @@
-
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from '../src/components/Header/Header';
+import AboutMe  from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+import Footer from '../src/components/Footer/Footer';
 
 function App() {
   return (
-    <div className="bucket-app">
-      <BucketList />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/about-me" element={<AboutMe />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/" element={<AboutMe />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
