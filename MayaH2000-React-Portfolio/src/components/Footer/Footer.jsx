6ba100c8a599e5cf08e4e51ac455699656cfbb73
MayaH2000 <101356128+MayaH2000@users.noreply.github.com>
2024-03-18 13:12:29 -0500

    added icons to portfolio

--- MayaH2000-React-Portfolio/src/components/Footer/Footer.jsx
+++ MayaH2000-React-Portfolio/src/components/Footer/Footer.jsx
@@ -3,9 +3,11 @@ function Footer () {
     <footer className="footer">
       <div className="social-links">
         <button><a href="https://github.com/MayaH2000/" target="_blank" rel="noopener noreferrer">
+          <span className="material-symbols-outlined">support_agent</span>
           GitHub
         </a></button>
         <button><a href="https://www.linkedin.com/in/maya-hernandez-51204b2b7/" target="_blank" rel="noopener noreferrer">
+        <span className="material-symbols-outlined">group</span>
           LinkedIn
         </a></button>
       </div>
--- MayaH2000-React-Portfolio/src/components/Header/Navigation.jsx
+++ MayaH2000-React-Portfolio/src/components/Header/Navigation.jsx
@@ -4,11 +4,12 @@ function Navigation() {
   return (
     <nav className='nav'>
       <ul>
-        <button><NavLink to="/about-me" >About Me</NavLink></button>
-        <button><NavLink to="/blog" >Blog</NavLink></button>
-        <button><NavLink to="/portfolio" >Portfolio</NavLink></button>
-        <button><NavLink to="/contact" >Contact</NavLink></button>
-        <button><NavLink to="/resume" >Resume</NavLink></button>
+        <button><span class="material-symbols-outlined">info</span><NavLink to="/about-me" >About Me</NavLink></button>
+        <button><span class="material-symbols-outlined">keyboard</span><NavLink to="/blog" >Blog</NavLink></button>
+        <button><span class="material-symbols-outlined">style</span><NavLink to="/portfolio" >Portfolio</NavLink></button>
+        <button> <span class="material-symbols-outlined">call</span> <NavLink to="/contact" >Contact</NavLink></button>
+        <button><span class="material-symbols-outlined">contract_edit</span><NavLink to="/resume" >Resume</NavLink></button>
+        
       </ul>
     </nav>
   );
