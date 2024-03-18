bd31202f4d582d428e087212a6ddcad287eec085
MayaH2000 <101356128+MayaH2000@users.noreply.github.com>
2024-03-18 13:47:58 -0500

    added new svg image ffor url

--- MayaH2000-React-Portfolio/src/components/Footer/Footer.jsx
+++ MayaH2000-React-Portfolio/src/components/Footer/Footer.jsx
@@ -1,16 +1,21 @@
-function Footer () {
+function Footer() {
   return (
     <footer className="footer">
       <div className="social-links">
-        <button><a href="https://github.com/MayaH2000/" target="_blank" rel="noopener noreferrer">
-          <span className="material-symbols-outlined">support_agent</span>
-          GitHub
-        </a></button>
-        <button><a href="https://www.linkedin.com/in/maya-hernandez-51204b2b7/" target="_blank" rel="noopener noreferrer">
-        <span className="material-symbols-outlined">group</span>
-          LinkedIn
-        </a></button>
+        <button>
+          <a href="https://github.com/MayaH2000/" target="_blank" rel="noopener noreferrer">
+            <span className="material-symbols-outlined">support_agent</span>
+            GitHub
+          </a>
+        </button>
+        <button>
+          <a href="https://www.linkedin.com/in/maya-hernandez-6a504216a/" target="_blank" rel="noopener noreferrer">
+            <span className="material-symbols-outlined">group</span>
+            LinkedIn
+          </a>
+        </button>
       </div>
+      <p>Created by Maya Hernandez 2024</p>
     </footer>
   );
 }
